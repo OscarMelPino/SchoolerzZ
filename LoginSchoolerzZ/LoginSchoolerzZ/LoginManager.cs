710ca4567c265b359a7ae7d718c45ceb69f13c4e
OscarMelPino <melgarejo.oscar17@gmail.com>
2022-01-31 20:43:56 +0100

    Base de login

---
+++ LoginSchoolerzZ/LoginSchoolerzZ/LoginManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSchoolerzZ
+{
+    public class LoginManager 
+    {
+        public String Username { get ; set;}
+        public String Password { get; set; }
+        public LoginManager(){ }
+        public LoginManager(char userType, String username, String pwd )
+        {
+            Username = userType + username;
+            Password = pwd;
+        }
+    }
+}
---
+++ LoginSchoolerzZ/LoginSchoolerzZ/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginSchoolerzZ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LoginManager manager;
+        private ArrayList baseLogin = new();
+        private ArrayList inputLogin = new();
+        private Char[] letterOption = { 'S', 'P', 'T', 'A' };
+        public MainWindow()
+        {
+            InitializeComponent();
+            baseLogin.Add(UserTypeContainer);
+            baseLogin.Add(ButtonNextContainer);
+
+            inputLogin.Add(InputContainer);
+            inputLogin.Add(ButtonLoginBackContainer);
+        }
+        public void UserTypeSet(object sender, RoutedEventArgs e)
+        {
+            if (ComboboxUserType.SelectedItem is null)
+            {
+                MessageBox.Show("No puede estar vacío.");
+                return;
+            }
+            foreach (StackPanel element in inputLogin)
+            {
+                element.Visibility = Visibility.Visible;
+            }
+            foreach (StackPanel element in baseLogin)
+            {
+                element.Visibility = Visibility.Collapsed;
+            }
+        }
+        public void MakeLogin(object sender, RoutedEventArgs e)
+        {
+            String un = !String.IsNullOrEmpty(UserInput.Text.ToString()) ? UserInput.Text.ToString() : null;
+            String pwd = !String.IsNullOrEmpty(PasswordInput.Password.ToString()) ? PasswordInput.Password.ToString() : null;
+            if (un is null || pwd == null) return;
+            manager = new(letterOption[ComboboxUserType.SelectedIndex], un, GetMD5(pwd));
+        }
+        public void Back(object sender, RoutedEventArgs e)
+        {
+            foreach (StackPanel element in baseLogin)
+            {
+                element.Visibility = Visibility.Visible;
+            }
+            foreach (StackPanel element in inputLogin)
+            {
+                element.Visibility = Visibility.Collapsed;
+            }
+        }
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = MD5CryptoServiceProvider.Create();
+            ASCIIEncoding encoding = new ();
+            byte[] stream = null;
+            StringBuilder sb = new ();
+            stream = md5.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+    }
+}
+
