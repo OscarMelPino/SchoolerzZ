639fe345de62d1fb4adb7852b5a65c311c338210
Alejandro Velasco Guerrero <alejandro.velasco.guerrero@alumnojoyfe.iepgroup.es>
2022-02-23 20:17:41 +0100

    Más bonito

--- LoginSchoolerzZ/obj/Debug/net5.0-windows/App.g.i.cs
+++ LoginSchoolerzZ/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace LoginSchoolerzZ {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/LoginSchoolerzZ;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LoginSchoolerzZ;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
